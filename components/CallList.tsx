9e0c2e32a444fe264071f85abb985aca470048cd
smanan <smanan0002@gmail.com>
2024-07-30 11:40:53 +0530

    wrong import files corrected

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -3,10 +3,10 @@
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 
 import Loader from './Loader';
-import { useGetCalls } from '@/hooks/useGetCalls';
-import MeetingCard from './MeetingCard';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { useGetCalls } from '@/hooks/useGetCalls';
+import MeetingCard from './MeetingCard';
 
 const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   const router = useRouter();

02126022e7a034fe1b0ef44ed6c89c3df6eaedc3
smanan <smanan0002@gmail.com>
2024-07-31 23:08:39 +0530

    corrected img logo

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,19 +1,16 @@
-"use client";
+'use client';
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { sidebarLinks } from "@/constants";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
 
 const MobileNav = () => {
   const pathname = usePathname();
+
   return (
     <section className="w-full max-w-[264px]">
       <Sheet>
@@ -22,7 +19,7 @@ const MobileNav = () => {
             src="/icons/hamburger.svg"
             width={36}
             height={36}
-            alt="hamburger"
+            alt="hamburger icon"
             className="cursor-pointer sm:hidden"
           />
         </SheetTrigger>
@@ -32,33 +29,35 @@ const MobileNav = () => {
               src="/icons/logo.svg"
               width={32}
               height={32}
-              alt="logo"
-              className="max-sm:size-10"
+              alt="yoom logo"
             />
-            <p className="text-[26px] font-extrabold text-white ">zoom</p>
+            <p className="text-[26px] font-extrabold text-white">YOOM</p>
           </Link>
-          <div className="flex h-[cal(100vh-72px)] flex-col justify-between overflow-y-auto">
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="flex h-full flex-col gap-6 pt-16 text-white">
-                {sidebarLinks.map((link) => {
-                  const isActive = pathname === link.route;
+              <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
+
                   return (
-                    <SheetClose asChild key={link.route}>
+                    <SheetClose asChild key={item.route}>
                       <Link
-                        href={link.route}
-                        key={link.label}
+                        href={item.route}
+                        key={item.label}
                         className={cn(
-                          "flex gap-4 items-center p-4 rounded-lg w-full max-w-60",
-                          { "bg-blue-1": isActive }
+                          'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+                          {
+                            'bg-blue-1': isActive,
+                          }
                         )}
                       >
                         <Image
-                          src={link.imgUrl}
-                          alt={link.label}
+                          src={item.imgURL}
+                          alt={item.label}
                           width={20}
                           height={20}
                         />
-                        <p className="font-semibold">{link.label}</p>
+                        <p className="font-semibold">{item.label}</p>
                       </Link>
                     </SheetClose>
                   );
